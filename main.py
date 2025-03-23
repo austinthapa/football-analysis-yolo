9391342f6d3f476207f9e78dc46b01e9999585a0
Anil Thapa <anilthapa1.at23@gmail.com>
2025-03-23 02:37:25 -0500

    feat: add read video frames and save it

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-if __name__ == "__main__":
-    main()
+from utils import read_video, save_video
 
 def main():
-    pass
+    
+    # Read video
+    video_path = "/Users/anilthapa/football-analysis-yolo/input_videos/input_video.mp4"
+    video_frames = read_video(video_path)
+    
+    # Run the tracker
+    
+    
+    # Save video
+    save_video(video_frames, '/Users/anilthapa/football-analysis-yolo/output_videos/output.avi')
+
+if __name__ == "__main__":
+    main()
---
+++ utils/video_utils.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+def read_video(file_path):
+    capture = cv.VideoCapture(file_path)
+    frames = []
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        frames.append(frame)
+    return frames
+
+def save_video(output_frames, output_path):
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    out = cv.VideoWriter(output_path, fourcc, 24, (output_frames[0].shape[1], output_frames[0].shape[0]))
+    for frame in output_frames:
+        out.write(frame)
+    out.release()
