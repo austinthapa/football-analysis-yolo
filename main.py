167b6e97a07d210213331f6e6b1d586eb4dc78a3
Anil Thapa <anilthapa1.at23@gmail.com>
2025-03-25 05:00:42 -0500

    refactor: add tracker to videos

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from trackers import Tracker
 def main():
     
     # Read video
-    video_path = "/Users/anilthapa/football-analysis-yolo/input_videos/input_video.mp4"
+    video_path = "/Users/anilthapa/football-analysis-yolo/input_videos/demo.mp4"
     video_frames = read_video(video_path)
     
     # Initialize the Tracker
     model_path = '/Users/anilthapa/football-analysis-yolo/models/best.pt'
     tracker = Tracker(model_path)
-    tracks = tracker.get_object_track(video_frames)
-    
+    tracks = tracker.get_object_track(video_frames, read_from_stub=True, stub_path='/Users/anilthapa/football-analysis-yolo/stubs/tracks_stub.pkl')
+    print(tracks['players'][5][1])
     # Save video
     save_video(video_frames, '/Users/anilthapa/football-analysis-yolo/output_videos/output.avi')
 
