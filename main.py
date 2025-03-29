263aaebbef98829a4aea2249e8e83c13c384915b
Anil Thapa <anilthapa1.at23@gmail.com>
2025-03-29 02:02:40 -0500

    refactor: changes to file paths

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from trackers import Tracker
 def main():
     
     # Read video
-    video_path = "/Users/anilthapa/football-analysis-yolo/input_videos/demo.mp4"
+    video_path = "/Users/anilthapa/football-analysis-yolo/input_videos/input_video.mp4"
     video_frames = read_video(video_path)
     
     # Initialize the Tracker
     model_path = '/Users/anilthapa/football-analysis-yolo/models/best.pt'
     tracker = Tracker(model_path)
-    tracks = tracker.get_object_track(video_frames, read_from_stub=True, stub_path='/Users/anilthapa/football-analysis-yolo/stubs/tracks_stub.pkl')
+    tracks = tracker.get_object_track(video_frames, read_from_stub=False, stub_path='/Users/anilthapa/football-analysis-yolo/stubs/tracks_stub.pkl')
     
     # Draw annotations 
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
     
     # Save video
-    save_video(output_video_frames, '/Users/anilthapa/football-analysis-yolo/output_videos/output.avi')
+    save_video(output_video_frames, '/Users/anilthapa/football-analysis-yolo/output_videos/output.mp4')
 
 if __name__ == "__main__":
     main()
