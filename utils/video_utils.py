1f4e0ce56519df099d94db1f6567d019a5bdaf56
Anil Thapa <anilthapa1.at23@gmail.com>
2025-04-02 22:29:50 -0500

    chore: fix the output video format

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -11,8 +11,8 @@ def read_video(file_path):
     return frames
 
 def save_video(output_frames, output_path):
-    fourcc = cv.VideoWriter_fourcc(*'mp4v')
-    out = cv.VideoWriter(output_path, fourcc, 24, (output_frames[0].shape[1], output_frames[0].shape[0]))
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    out = cv.VideoWriter(output_path, fourcc, 24, (output_frames[4].shape[1], output_frames[4].shape[0]))
     for frame in output_frames:
         out.write(frame)
     out.release()
